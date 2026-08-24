cc9634443f5c395595cc95b1ecc835272cc79c7b
mukhiddintolkinov2-arch <mukhiddintolkinov2@gmail.com>
2026-08-24 21:17:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def start_message(message):
 def handle_message(message):
     try:
         response = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama-3.1-8b-instant",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": message.text}
